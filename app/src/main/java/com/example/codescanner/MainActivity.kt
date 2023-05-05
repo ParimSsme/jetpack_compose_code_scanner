e4d86afc0f966f0b129f8b1dd9d0324c57630cf9
ParimSsme <parima.ssme@gmail.com>
2023-05-05 10:59:27 +0430

    work on adding barcode scanner

--- app/src/main/java/com/example/codescanner/MainActivity.kt
+++ app/src/main/java/com/example/codescanner/MainActivity.kt
@@ -1,28 +1,67 @@
 package com.example.codescanner
 
+import android.Manifest
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import com.example.codescanner.ui.theme.CodeScannerTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
+@ExperimentalPermissionsApi
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CodeScannerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Bottom
+                    ) {
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        val cameraPermissionState = rememberPermissionState(permission = Manifest.permission.CAMERA)
+
+                        Button(
+                            onClick = {
+                                cameraPermissionState.launchPermissionRequest()
+                            }
+                        ) {
+                            Text(text = "Camera Permission")
+                        }
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        CameraPreview()
+                    }
+
+
                 }
             }
         }
@@ -30,14 +69,64 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun CameraPreview() {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    var preview by remember { mutableStateOf<Preview?>(null) }
+    val barCodeVal = remember { mutableStateOf("") }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CodeScannerTheme {
-        Greeting("Android")
-    }
+    AndroidView(
+        factory = { AndroidViewContext ->
+            PreviewView(AndroidViewContext).apply {
+                this.scaleType = PreviewView.ScaleType.FILL_CENTER
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                )
+                implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+            }
+        },
+        modifier = Modifier.fillMaxSize().padding(50.dp),
+        update = { previewView ->
+            val cameraSelector: CameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+            val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+            val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
+                ProcessCameraProvider.getInstance(context)
+
+            cameraProviderFuture.addListener({
+                preview = Preview.Builder().build().also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+                val barcodeAnalyser = BarCodeAnalyser { barcodes ->
+                    barcodes.forEach { barcode ->
+                        barcode.rawValue?.let { barcodeValue ->
+                            barCodeVal.value = barcodeValue
+                            Toast.makeText(context, barcodeValue, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+                val imageAnalysis: ImageAnalysis = ImageAnalysis.Builder()
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                    .build()
+                    .also {
+                        it.setAnalyzer(cameraExecutor, barcodeAnalyser)
+                    }
+
+                try {
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        preview,
+                        imageAnalysis
+                    )
+                } catch (e: Exception) {
+                    Log.d("TAG", "CameraPreview: ${e.localizedMessage}")
+                }
+            }, ContextCompat.getMainExecutor(context))
+        }
+    )
 }
