e92b96f575220f62e98a7eab49f265fb9b7cdb53
ParimSsme <parima.ssme@gmail.com>
2023-05-05 10:57:25 +0430

    work on adding barcode scanner

--- app/src/main/java/com/example/codescanner/BarcodeAnalyser.kt
+++ app/src/main/java/com/example/codescanner/BarcodeAnalyser.kt
@@ -1,38 +1,49 @@
 package com.example.codescanner
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
+import java.util.concurrent.TimeUnit
 
-class BarcodeAnalyser(
-    val callback: () -> Unit
-) : ImageAnalysis.Analyzer {
-    @SuppressLint("UnsafeOptInUsageError")
-    override fun analyze(imageProxy: ImageProxy) {
-        val options = BarcodeScannerOptions.Builder()
-            .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
-            .build()
+@SuppressLint("UnsafeOptInUsageError")
+class BarCodeAnalyser(
+    private val onBarcodeDetected: (barcodes: List<Barcode>) -> Unit,
+): ImageAnalysis.Analyzer {
+    private var lastAnalyzedTimeStamp = 0L
 
-        val scanner = BarcodeScanning.getClient(options)
-        val mediaImage = imageProxy.image
-        mediaImage?.let {
-            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+    override fun analyze(image: ImageProxy) {
+        val currentTimestamp = System.currentTimeMillis()
+        if (currentTimestamp - lastAnalyzedTimeStamp >= TimeUnit.SECONDS.toMillis(1)) {
+            image.image?.let { imageToAnalyze ->
+                val options = BarcodeScannerOptions.Builder()
+                    .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
+                    .build()
+                val barcodeScanner = BarcodeScanning.getClient(options)
+                val imageToProcess = InputImage.fromMediaImage(imageToAnalyze, image.imageInfo.rotationDegrees)
 
-            scanner.process(image)
-                .addOnSuccessListener { barcodes ->
-                    if (barcodes.size > 0) {
-                        callback()
+                barcodeScanner.process(imageToProcess)
+                    .addOnSuccessListener { barcodes ->
+                        if (barcodes.isNotEmpty()) {
+                            onBarcodeDetected(barcodes)
+                        } else {
+                            Log.d("TAG", "analyze: No barcode Scanned")
+                        }
                     }
-                }
-                .addOnFailureListener {
-                    // Task failed with an exception
-                    // ...
-                }
+                    .addOnFailureListener { exception ->
+                        Log.d("TAG", "BarcodeAnalyser: Something went wrong $exception")
+                    }
+                    .addOnCompleteListener {
+                        image.close()
+                    }
+            }
+            lastAnalyzedTimeStamp = currentTimestamp
+        } else {
+            image.close()
         }
-        imageProxy.close()
     }
 }
